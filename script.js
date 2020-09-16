4afe2ea0066f33109a650b18ceed5660db93310a
Dark-Fire998 <50399948+Dark-Fire998@users.noreply.github.com>
2020-09-16 23:10:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//   begin
+
+const heightHeader = window.innerHeight * 0.5;
+document.getElementById("header").style.height = heightHeader+"px";
+document.getElementById("star").style.margin = `-${heightHeader * 0.5}px 0 0 0`;
+
